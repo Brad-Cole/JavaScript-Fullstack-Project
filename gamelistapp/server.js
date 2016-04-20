2467b4dd12d9484faa074e78ed5cf93b596364b5
Brad Cole <blcole1@hotmail.com>
2016-04-19 20:00:15 -0400

    Tidied up code formatting.

--- gamelistapp/server.js
+++ gamelistapp/server.js
@@ -7,39 +7,53 @@ var db = mongojs('gamedb', ['gamedb']);
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-app.get('/gamedb', function (req, res){
-		db.gamedb.find(function (err, docs){
-		res.json(docs);
-	});
+app.get('/gamedb', function(req, res) {
+  db.gamedb.find(function(err, docs) {
+    res.json(docs);
+  });
 });
 
-app.post('/gamedb', function (req, res) {
-	db.gamedb.insert(req.body, function(err, doc)  {
-		res.json(doc);
-	})
+app.post('/gamedb', function(req, res) {
+  db.gamedb.insert(req.body, function(err, doc) {
+    res.json(doc);
+  })
 });
 
-app.delete('/gamedb/:id', function (req, res) {
-	var id = req.params.id;
-	db.gamedb.remove({_id: mongojs.ObjectId(id)}, function (err, doc)  {
-		res.json(doc);
-	})
+app.delete('/gamedb/:id', function(req, res) {
+  var id = req.params.id;
+  db.gamedb.remove({
+    _id: mongojs.ObjectId(id)
+  }, function(err, doc) {
+    res.json(doc);
+  })
 });
 
-app.get('/gamedb/:id', function (req, res) {
-	var id = req.params.id;
-	db.gamedb.findOne({_id: mongojs.ObjectId(id)}, function (err, doc)  {
-		res.json(doc);
-	});
+app.get('/gamedb/:id', function(req, res) {
+  var id = req.params.id;
+  db.gamedb.findOne({
+    _id: mongojs.ObjectId(id)
+  }, function(err, doc) {
+    res.json(doc);
+  });
 });
 
-app.put('/gamedb/:id', function (req, res) {
-	var id = req.params.id;
-	db.gamedb.findAndModify({query: {_id: mongojs.ObjectId(id)},
-		update: {$set: {game: req.body.game, system: req.body.system, value: req.body.value}},
-		new: true}, function (err, doc) {
-			res.json(doc);
-		});
+app.put('/gamedb/:id', function(req, res) {
+  var id = req.params.id;
+  db.gamedb.findAndModify({
+    query: {
+      _id: mongojs.ObjectId(id)
+    },
+    update: {
+      $set: {
+        game: req.body.game,
+        system: req.body.system,
+        value: req.body.value
+      }
+    },
+    new: true
+  }, function(err, doc) {
+    res.json(doc);
+  });
 });
 
 app.listen(3000);
